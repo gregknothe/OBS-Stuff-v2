7c400a9ce7a145415e407ce1d7443e21cf48ace8
gregknothe <asome26@gmail.com>
2024-10-25 23:02:53 -0700

    10-24-24 (Part 4)

--- cardUpdate.py
+++ cardUpdate.py
@@ -3,10 +3,14 @@ import os
 
 def userCardFiles():
     df = pd.read_csv("cardList.csv", sep="|",).fillna("")
+    userList = []
     for user in list(df["owner"].unique()):
         print(user)
+        userList.append(user)
         userDF = df[df["owner"]==user]
-        userDF.to_csv("E:/Various Programs/Coding Projects/OBS Stuff v2/userDataFrames/"+user+".csv", sep="|", header=False)
+        userDF.to_csv("E:/Various Programs/Coding Projects/OBS Stuff v2/userDataFrames/"+user+".csv", sep="|", header=False, index=False)
+    userListDF = pd.DataFrame(filter(None, userList))
+    userListDF.to_csv("E:/Various Programs/Coding Projects/OBS Stuff v2/userDataFrames/userList.csv", sep="|", header=False, index=False)
     return
 
 userCardFiles()
